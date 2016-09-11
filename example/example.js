795047908b50109c9e60d7069089abafceba1215
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-09-10 17:53:57 -0700

    Expanded the example.
    - Using Express JS server.
    - Display JSON profile info on web page.

--- example/example.js
+++ example/example.js
@@ -1,6 +1,26 @@
+// Constants
+const PORT = process.env.PORT || 5000;
+const CREDENTIALS_CONFIG_FILE = __dirname + '/config.json';
+
+// Global Variables
+var app = require('express')();
+var profile = {};
+
 require('../linkedin-scraper')(
-	__dirname + '/config.json',
-	function(profileInfo) {
-		console.log(profileInfo);
+	CREDENTIALS_CONFIG_FILE, 
+	
+	function(profileInfo) { 
+		profile = profileInfo;
+				
+		// Start this server only after we get back information
+		// from the LinkedIn scraper.
+		app.listen(PORT, function() {
+			console.log('"node-linkedin-scraper-example"',
+				"is running on port", PORT);
+		});
 	}
 );
+
+app.get('/', function(request, response) {
+	response.json(profile);
+});
