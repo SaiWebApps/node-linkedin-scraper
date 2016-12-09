b6d75bcfd2e5b552420c8fdec9f8462f42d2cea0
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-12-08 21:06:31 -0500

    Refined example to reflect recent optimizations.

--- example/example.js
+++ example/example.js
@@ -4,69 +4,63 @@ const CREDENTIALS_CONFIG_FILE = __dirname + '/config.json';
 
 // Global Variables
 var app = require('express')();
-var myProfile = {};
-var myConnections = {};
+var linkedinScraper = require('../index');
 
-// Configure app.
+// Express App Config
 app.set('view engine', 'ejs');
 
-// Use node-linkedin-scraper to gather information about your (current
-// user's) LinkedIn account.
-var linkedinScraper = require('../index');
-linkedinScraper.me(CREDENTIALS_CONFIG_FILE, 
-	function(profileInfo, connectionsInfo) {
-		myProfile = profileInfo;
-		myConnections = connectionsInfo;
-
-		// Start the server only after we've received all our information.
-		app.listen(PORT, function() {
-			console.log('node-linkedin-scraper-example is',
-				'running on port', PORT);
-		});
-	}
-);
-
 // URL Routing
-app.get('/me', function(request, response) {
-	response.render('index', {
-		'profile': myProfile,
-		'connections': myConnections,
-		'showProfile': true,
-		'showConnections': true,
-		'title': myProfile.fullName + ' @ LinkedIn'
-	});
-});
-
-app.get('/me/connections', function(request, response) {
-	response.render('index', {
-		'connections': myConnections,
-		'showProfile': false,
-		'showConnections': true,
-		'title': myProfile.fullName + ' @ LinkedIn - Connections'
-	});
-});
-
-app.get('/profile', function(request, response) {
-	if (!('profileUrl' in request.query)) {
-		response.render('index', {
-			'profile': myProfile,
-			'showProfile': true,
-			'showConnections': false,
-			'title': myProfile.fullName + ' @ LinkedIn - Profile'
-		});
-		return;
-	}
-
-	linkedinScraper.profile(
+app.get('/', function(request, response) {
+	linkedinScraper.me(
 		CREDENTIALS_CONFIG_FILE, 
-		request.query.profileUrl,
-		function(profile) {
+		function(profileInfo, connectionsInfo) {
 			response.render('index', {
-				'profile': profile,
+				'profile': profileInfo,
+				'connections': connectionsInfo,
 				'showProfile': true,
-				'showConnections': false,
-				'title': profile.fullName + ' @ LinkedIn - Profile'
+				'showConnections': true,
+				'title': profileInfo.fullName + ' @ LinkedIn'
 			});
 		}
 	);
+});
+
+app.get('/connections', function(request, response) {
+	linkedinScraper.me.connections(
+		CREDENTIALS_CONFIG_FILE,
+		function(connectionsInfo) {
+			response.render('index', {
+				'connections': myConnections,
+				'showProfile': false,
+				'showConnections': true,
+				'title': myProfile.fullName + ' @ LinkedIn - Connections'
+			});
+		}
+	);
+});
+
+app.get('/profile', function(request, response) {
+	var processProfileInfo = function(profileInfo) {
+		const TITLE_SUFFIX = ' @ LinkedIn - Profile';
+		response.render('index', {
+			'profile': profileInfo,
+			'showProfile': true,
+			'showConnections': false,
+			'title': profileInfo.fullName + TITLE_SUFFIX
+		});
+	};
+
+	if (!('profileUrl' in request.query)) {
+		linkedinScraper.me.profile(CREDENTIALS_CONFIG_FILE, 
+			processProfileInfo);
+		return;
+	}
+	linkedinScraper.profile(CREDENTIALS_CONFIG_FILE, 
+		request.query.profileUrl, processProfileInfo);
+});
+
+// Start server.
+app.listen(PORT, function() {
+	console.log('node-linkedin-scraper-example is',
+		'running on port', PORT);
 });
