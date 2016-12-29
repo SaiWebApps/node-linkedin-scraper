0a971b6a0200191504c7de8181f0452b3fe071f7
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-12-29 07:53:19 -0500

    Fixed get-education for others' profiles.

--- tasks/get-profile/get-education.js
+++ tasks/get-profile/get-education.js
@@ -5,51 +5,85 @@ module.exports = function(profileInfo, browser, asyncCallback) {
 
 			// Add each education node's information as a JSON
 			// object to output.
-			var educationNodes = document.querySelectorAll(
-				'#background-education > div.entity-container > div.entity ' +
-				' > div.edit-action-area');
+			var educationNodes = document.querySelectorAll([
+				'#background-education > div.entity-container >',
+				'div.entity > div.edit-action-area'
+			].join(' '));
+			if (educationNodes.length === 0) {
+				educationNodes = document.querySelectorAll(
+					'#background-education > div > div > div');
+			}
 
 			educationNodes.forEach(function(educationNode) {
 				// Utility functions
-				var get = (selector) => educationNode.querySelector(selector);
-				var getText = function(selector) {
-					var targetNode = get(selector);
+				var get = function(selector, altSelector) {
+					return educationNode.querySelector(selector) ||
+						educationNode.querySelector(altSelector);
+				};
+				
+				var getText = function(selector, altSelector) {
+					var targetNode = get(selector, altSelector);
 					if (!targetNode) {
 						return null;
 					}
 					return targetNode.innerText;
 				};
-				var getImgSrc = function(selector) {
-					var targetNode = get(selector);
+				
+				var getImgSrc = function(selector, altSelector) {
+					var targetNode = get(selector, altSelector);
 					if (!targetNode) {
 						return null;
 					}
 					return targetNode.src;
-				}
-				var getAll = (selector) => 
-					educationNode.querySelectorAll(selector);
+				};
+				
+				// For the first selector with matches, return the
+				// corresponding list of nodes.
+				var getAll = function(selector) {
+					var target = educationNode.querySelectorAll(selector);
+					if (arguments.length === 1 || target.length > 0) {
+						return target;
+					}
+
+					for (var i = 1; i < arguments.length; i++) {
+						var arg = arguments[i];
+						target = educationNode.querySelectorAll(arg);
+						
+						// We're done if target has items because we
+						// found matches for this selector, arg.
+						if (arg && target && target.length > 0) {
+							break;
+						}
+					}
+					return target;
+				};
+
 				var clean = function(str) {
 					if (!str) {
 						return str;
 					}
 					return str.trim().replace(/(^[\W])|(,$)/g, '').trim();
-				}
+				};
 
 				// Extract basic details about this education entry.
 				var educationInfo = {
-					title: getText('div > header > h4 > div > span'),
+					title: getText('div > header > h4 > div > span',
+						'div > header > h4 > a'),
 					degree: clean(getText('div > header > h5 > span >' +
-						' span.degree')),
+						' span.degree', 'div > header > h5 > span.degree')),
 					major: clean(getText('div > header > h5 > span >' +
-						' span.major')),
+						' span.major', 'div > header > h5 > span.major > a')),
 					grade: getText('div > header > h5 > span >' +
-						' span:nth-child(3)'),
+						' span:nth-child(3)', 'div > header > h5 > span.grade'),
 					startTime: getText('div > div.date-header-field.' +
-						'field > span > time:nth-child(1)'),
+						'field > span > time:nth-child(1)', 
+						'div > span > time:nth-child(1)'),
 					endTime: clean(getText('div > div.date-header-field.' +
-						'field > span > time:nth-child(2)')),
-					description: getText('div > p > span'),
-					imgUrl: getImgSrc('div > div > h5 > span > strong > img')
+						'field > span > time:nth-child(2)',
+						'div > span > time:nth-child(2)')),
+					description: getText('div > p > span', 'div > p'),
+					imgUrl: getImgSrc('div > div > h5 > span > strong > img',
+						'div > a > h5 > span > strong > img')
 				};
 
 				// Extract related projects and courses info for this 
@@ -58,27 +92,31 @@ module.exports = function(profileInfo, browser, asyncCallback) {
 				getAll('div > dl > dd:nth-child(2) > ul > li > h6')
 					.forEach(function(projectNode) {
 						educationInfo.associatedProjects.push(
-							projectNode.innerHTML);
+							projectNode.innerText);
 					});
 
 				const PREFIX = 'div > dl > dd:nth-child(';
-				const SUFFIX = ') > ul > li > h6';
+				const SUFFIX = ') > ul > li';
+				const OPTIONAL_SUFFIX = ' > h6';
 
-				var courseNodes = getAll(PREFIX + 6 + SUFFIX);
+				var courseNodes = getAll(PREFIX + 6 + SUFFIX,
+					PREFIX + 6 + SUFFIX + OPTIONAL_SUFFIX);
 				var awardNodes = getAll(PREFIX + 4 + SUFFIX);
 				if (courseNodes.length === 0) {
-					courseNodes = getAll(PREFIX + 4 + SUFFIX);
+					courseNodes = getAll(PREFIX + 4 + SUFFIX,
+						PREFIX + 4 + SUFFIX + OPTIONAL_SUFFIX,
+						'div > dl > dd > ul > li');
 					awardNodes = [];
 				}
-				
+
 				educationInfo.associatedCourses = [];
 				courseNodes.forEach(function(courseNode) {
-					educationInfo.associatedCourses.push(courseNode.innerHTML);
+					educationInfo.associatedCourses.push(courseNode.innerText);
 				});
 
 				educationInfo.associatedAwards = [];
 				awardNodes.forEach(function(awardNode) {
-					educationInfo.associatedAwards.push(awardNode.innerHTML);
+					educationInfo.associatedAwards.push(awardNode.innerText);
 				});
 
 				// Add JSON object with current education entry's 
