9e84cdf516405f32b32c3000589dcc4b13601765
Sairam Krishnan <sairambkrishnan@gmail.com>
2017-01-26 07:29:47 -0500

    Fixed get-certifications for others' profiles.

--- tasks/get-profile/get-certifications.js
+++ tasks/get-profile/get-certifications.js
@@ -3,39 +3,72 @@ module.exports = function(profileInfo, browser, asyncCallback) {
 		.evaluate(function() {
 			var output = [];
 
+			// Utility functions
+			var get = function() {
+				var querySelector = (sel) => document.querySelector(sel);
+				var startIdx = 0;
+				if (typeof(arguments[0]) === 'object') {
+					querySelector = (sel) => arguments[0].querySelector(sel);
+					startIdx = 1;
+				}
+
+				for (var i = startIdx; i < arguments.length; i++) {
+					var query = arguments[i];
+					var targetNode = querySelector(query);
+					if (targetNode) {
+						return targetNode;
+					}
+				}
+				return null;
+			};
+			var getText = function() {
+				var targetNode = get.apply(this, arguments);
+				return (targetNode) ? targetNode.innerText : null;
+			};
+			var getImgSrc = function() {
+				var targetNode = get.apply(this, arguments);
+				return (targetNode) ? targetNode.src : null;
+			};
+			var getAll = function() {
+				var querySelectorAll = (sel) => document.querySelectorAll(sel);
+				var startIdx = 0;
+				if (typeof(arguments[0]) === 'object') {
+					querySelectorAll = (sel) => arguments[0].querySelectorAll(sel);
+					startIdx = 1;
+				}
+
+				for (var i = startIdx; i < arguments.length; i++) {
+					var query = arguments[i];
+					var targetNode = querySelectorAll(query);
+					if (targetNode && targetNode.length > 0) {
+						return targetNode;
+					}
+				}
+				return [];
+			};
+
 			// Add each certification node's information as a JSON
 			// object to output.
-			var certificationNodes = document.querySelectorAll(
-				'#background-certifications > div.entity-container' +
-				' > div.entity > div.edit-action-area');
+			var certificationNodes = getAll(
+				'#background-certifications > div.entity-container > ' +
+					'div.entity > div.edit-action-area',
+				'#background-certifications > div'
+			);
 
 			certificationNodes.forEach(function(certNode) {
-				// Utility functions
-				var get = (selector) => certNode.querySelector(selector);
-				var getText = function(selector) {
-					var targetNode = get(selector);
-					if (!targetNode) {
-						return null;
-					}
-					return targetNode.innerText;
-				};
-				var getImgSrc = function(selector) {
-					var targetNode = get(selector);
-					if (!targetNode) {
-						return null;
-					}
-					return targetNode.src;
-				}
-				var getAll = (selector) => certNode.querySelectorAll(selector);
-
 				// Extract basic details about this certification entry.
 				var certInfo = {
-					title: getText('div > header > h4 > div > span'),
-					imgUrl: getImgSrc('div > header > h5.section-logo >' +
-						' span > strong > img'),
-					certAuthority: getText('div > header > h5.' + 
-						'sub-header-field.field > span'),
-					timePeriod: getText('div > div > span')
+					title: getText(certNode,
+						'div > hgroup > h4 > a',
+						'div > header > h4 > div > span'),
+					imgUrl: getImgSrc(certNode,
+						'div > hgroup > h5.certification-logo > a ' +
+							'> span > strong > img',
+						'div > header > h5.section-logo > span > strong > img'),
+					certAuthority: getText(certNode,
+						'div > hgroup > h5 > span > strong > a',
+						'div > header > h5.sub-header-field.field > span'),
+					timePeriod: getText(certNode, 'div > div > span', 'div > span')
 				};
 
 				// Add JSON object with current certification entry's 
