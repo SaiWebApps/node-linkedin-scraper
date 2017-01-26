df5071a150b6115035278adc28110b478c02fa9d
Sairam Krishnan <sairambkrishnan@gmail.com>
2017-01-26 07:12:27 -0500

    Fixed get-experience for others' profiles.
    Also removed "associatedProjects" from get-experience output since
    subsection only showed excerpt of all relevant projects.
    Same for associatedRecommendations, associatedOrganizations, etc.,
    so I'm only focusing only the core experience info.

--- tasks/get-profile/get-experience.js
+++ tasks/get-profile/get-experience.js
@@ -3,48 +3,89 @@ module.exports = function(profileInfo, browser, asyncCallback) {
 		.evaluate(function() {
 			var output = [];
 
+			// Utility functions
+			var get = function() {
+				var querySelector = (sel) => document.querySelector(sel);
+				var startIdx = 0;
+				if (typeof(arguments[0]) === 'object') {
+					querySelector = (sel) => arguments[0].querySelector(sel);
+					startIdx = 1;
+				}
+
+				for (var i = startIdx; i < arguments.length; i++) {
+					var query = arguments[i];
+					var targetNode = querySelector(query);
+					if (targetNode) {
+						return targetNode;
+					}
+				}
+				return null;
+			};
+			var getText = function() {
+				var targetNode = get.apply(this, arguments);
+				return (targetNode) ? targetNode.innerText : null;
+			};
+			var getImgSrc = function() {
+				var targetNode = get.apply(this, arguments);
+				return (targetNode) ? targetNode.src : null;
+			};
+			var getAll = function() {
+				var querySelectorAll = (sel) => document.querySelectorAll(sel);
+				var startIdx = 0;
+				if (typeof(arguments[0]) === 'object') {
+					querySelectorAll = (sel) => arguments[0].querySelectorAll(sel);
+					startIdx = 1;
+				}
+
+				for (var i = startIdx; i < arguments.length; i++) {
+					var query = arguments[i];
+					var targetNode = querySelectorAll(query);
+					if (targetNode && targetNode.length > 0) {
+						return targetNode;
+					}
+				}
+				return [];
+			};
+
 			// Add each professional-experience node's information 
 			// as a JSON object to output.
-			var experienceNodes = document.querySelectorAll(
-				'#background-experience > div.entity-container > div.entity' +
-				' > div.edit-action-area');
+			const MY_PROFILE_SELECTOR = [
+				'#background-experience > div.entity-container',
+				'div.entity > div.edit-action-area'
+			].join('>');
+			var experienceNodes = getAll(MY_PROFILE_SELECTOR,
+				'#background-experience > div > div');
 
 			experienceNodes.forEach(function(experienceNode) {
-				// Utility functions
-				var get = (selector) => experienceNode.querySelector(selector);
-				var getText = function(selector) {
-					var targetNode = get(selector);
-					if (!targetNode) {
-						return null;
-					}
-					return targetNode.innerText;
-				};
-				var getImgSrc = function(selector) {
-					var targetNode = get(selector);
-					if (!targetNode) {
-						return null;
-					}
-					return targetNode.src;
-				}
-				var getAll = (selector) => 
-					experienceNode.querySelectorAll(selector);
-
 				// Extract basic details about this experience entry.
 				var experienceInfo = {
-					title: getText('div > header > h4 > div > span'),
-					company: getText('div > header > h5.field > span'),
-					timePeriod: getText('div > div > span > time'),
-					location: getText('div > div > span > span > span'),
-					description: getText('div > p > span'),
-					imgUrl: getImgSrc('div > header > h5.section-logo >' +
-						' span > strong > img')
+					title: getText(experienceNode, 
+						'div > header > h4 > div > span',
+						'div > header > h4 > a'),
+					company: getText(experienceNode,
+						'div > header > h5.field > span',
+						'div > header > h5 > span',
+						'div > header > h5 > a'),
+					timePeriod: getText(experienceNode,
+						'div > div > span'),
+					location: getText(experienceNode,
+						'div > div > span > span > span',
+						'div > div > span > span'),
+					description: getText(experienceNode, 
+						'div > p > span', 
+						'div > p'),
+					imgUrl: getImgSrc(experienceNode,
+						'div > header > h5.section-logo > span > strong > img',
+						'div > header > h5.experience-logo > a > span > strong > img')
 				};
-
-				// Extract names of projects associated with this experience.
-				experienceInfo.associatedProjects = [];
-				getAll('div > dl > dd > ul > li > h6').forEach(function(node) {
-					experienceInfo.associatedProjects.push(node.innerText);
-				});
+				// If we successfully extracted a timeStr, then currently,
+				// it includes time AND locale info, which are separated by
+				// ) at end of the time info.
+				var timeStr = experienceInfo.timePeriod;
+				if (timeStr) {
+					timeStr = timeStr.split(')')[0] + ')';
+					experienceInfo.timePeriod = timeStr;
+				}
 
 				// Add JSON object with current experience entry's 
 				// details to output.
